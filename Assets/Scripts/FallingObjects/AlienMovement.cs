e7f0d62fd3ee1fdc987f86fb40a0eb17d67fddb3
VerriVerri <aperezantanes@gmail.com>
2025-01-15 19:35:25 +0100

    Aded QOL changes to the objects minigame

--- Assets/Scripts/FallingObjects/AlienMovement.cs
+++ Assets/Scripts/FallingObjects/AlienMovement.cs
@@ -3,21 +3,27 @@ using UnityEngine;
 public class AlienMovement : MonoBehaviour
 {
     public float speed = 2.0f;
+    public float maxRange;
+    public GameObject alien;
+    public SpriteRenderer renderer;
+    public float initialXPosition;
+    public float x;
     void Start()
     {
-        
+        initialXPosition = -alien.transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
+        x = alien.transform.position.x + initialXPosition;
         float entradaHorizontal = 0;
 
-        if (Input.GetKey(KeyCode.A))
+        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) && (alien.transform.position.x + initialXPosition) > -maxRange)
         {
             entradaHorizontal = -1; 
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if ((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) && (alien.transform.position.x + initialXPosition) < maxRange)
         {
             entradaHorizontal = 1; 
         }
